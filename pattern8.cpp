91bdb9dec75be8a5e373bdd9afdbcfbcdbd3f014
Prasad-R-Patil <prasadpatil24212@gmail.com>
2024-08-27 12:07:24 +0530

    cmt

---
+++ pattern8.cpp
@@ -0,0 +1,47 @@
+/*
+
+
+
+
+
+*/
+
+
+
+#include<iostream>
+using namespace std;
+ 
+ 
+int main()
+{
+
+    int n = 0 ;
+    cout<<"enter value of n  => ";
+    cin>>n;
+
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(j<i)
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"*";
+            }
+          
+        }
+        for(int k=n-i; k>=1; k--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+ 
+ 
+ 
+ return 0;
+}
---
+++ pattern9.cpp
@@ -0,0 +1,44 @@
+/*
+
+
+
+
+
+*/
+
+
+
+#include<iostream>
+using namespace std;
+ 
+ 
+int main()
+{
+
+    int n = 0 ;
+    cout<<"enter value of n  => ";
+    cin>>n;
+
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(j<i)
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"* ";
+            }
+
+          
+        }
+        cout<<endl;
+    }
+ 
+ 
+ 
+ return 0;
+}
